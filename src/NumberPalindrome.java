39810a1a7d82c3b28c8ab28c927e2685f6706d5b
Rohhian <kristianmiilen@gmail.com>
2021-10-12 16:35:12 +0300

    Initial commit

---
+++ src/NumberPalindrome.java
@@ -0,0 +1,16 @@
+public class NumberPalindrome {
+    public static boolean isPalindrome(int number) {
+        if (number < 0) {
+            number *= -1;
+        }
+        int reverse = 0;
+        int num = number;
+        while (num > 0) {
+            int lastDigit = num % 10;
+            reverse = reverse * 10;
+            reverse = reverse + lastDigit;
+            num = num / 10;
+        }
+        return number == reverse;
+    }
+}
